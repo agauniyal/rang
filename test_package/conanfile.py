6860881e7eed2dd0d766fc4e484351401391fa29
agauniyal <agauniyal@outlook.com>
2026-05-16 20:29:36 +0530

    Migrate to Conan 2 and fix doctest include

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,17 +1,30 @@
-from conans import ConanFile, Meson
 import os
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.meson import MesonToolchain, Meson
+from conan.tools.env import VirtualRunEnv
 
-class RangConan(ConanFile):
-    generators = "pkg_config"
-    exports_sources = "*"
+class RangTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "PkgConfigDeps", "VirtualRunEnv"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def layout(self):
+        from conan.tools.layout import basic_layout
+        basic_layout(self)
+
+    def generate(self):
+        tc = MesonToolchain(self)
+        tc.generate()
 
     def build(self):
         meson = Meson(self)
         meson.configure()
         meson.build()
 
-    def imports(self):
-        self.copy("*.hpp")
-
     def test(self):
-        self.run(".%svisualTest" % os.sep)
+        if can_run(self):
+            cmd = os.path.join(self.cpp.build.bindir, "visualTest")
+            self.run(cmd, env="conanrun")
